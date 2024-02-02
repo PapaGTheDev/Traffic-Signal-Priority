9394f67f975e6cceb9fe912163ce41c3cf86a9fb
PapaGTheDev <141927116+PapaGTheDev@users.noreply.github.com>
2024-02-02 11:07:31 -0500

    Update server.lua

--- server.lua
+++ server.lua
@@ -1,6 +1,6 @@
 
 -- Send request to change traffic light to all clients
-RegisterServerEvent("SmartTrafficLights:setLight")
-AddEventHandler("SmartTrafficLights:setLight", function(coords)
-    TriggerClientEvent("SmartTrafficLights:setLight", -1, coords)
+RegisterServerEvent("PapaGTraffic:setLight")
+AddEventHandler("PapaGTraffic:setLight", function(coords)
+    TriggerClientEvent("PapaGTraffic:setLight", -1, coords)
 end)
